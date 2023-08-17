2ffefc075119f3a3742095434911fc5778837491
Arpan semwal <68865320+arpan-semwal@users.noreply.github.com>
2023-08-17 14:33:33 +0530

    update chat.jsx

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -4,7 +4,7 @@ import Logo from "./Logo";
 import { UserContext } from "../Context/UserContext";
 
 const Chat = () => {
-    const [ ,setWs] = useState(null);
+    const [ws ,setWs] = useState(null);
     const [onlinePeople , setOnlinePeople] = useState([]);
     const [selectedUserId , setSelectedUserId] = useState(null);
     const [newMessgaeText , setNewMessageText] = useState('');
@@ -38,34 +38,43 @@ const Chat = () => {
     const onlinePeopleExcudingOurUser = {...onlinePeople};
     delete onlinePeopleExcudingOurUser[id];
    
+    function sendMessage(ev){
+      ev.preventDefault();
+      ws.send(JSON.stringify({
+        message:{
+          recipient:selectedUserId,
+          text:newMessgaeText,
+        }
+      }));
+    }
 
 
-
+    
   return (
     <div className="flex h-screen">
         <div className="bg-blue-100 w-1/3 ">
          <Logo/>
       {Object.keys(onlinePeopleExcudingOurUser).map(userId => (
-        <div
-        onClick={() => setSelectedUserId(userId)}
-        className={
-        "border-b border-gray-100 py-2 pl-4 flex items-center gap-2 cursor-pointer" +
-        (userId === selectedUserId ? ' bg-blue-200' : '') // Add background color class
+          <div
+         onClick={() => setSelectedUserId(userId)}
+         className={
+         "border-b border-gray-100 py-2 pl-4 flex items-center gap-2 cursor-pointer" +
+         (userId === selectedUserId ? ' bg-blue-200' : '') // Add background color class
       }
       key={userId}
     >
       {userId === selectedUserId && (
-  <div className="w-1 bg-blue-500 h-12 rounded-r-md"></div>
-)}
+       <div className="w-1 bg-blue-500 h-12 rounded-r-md"></div>
+    )}
 
-      <div className="flex gap-2 py-2 pl-4 items-center">
-          <Avatar username={onlinePeople[userId]} userId={userId} />
-          <span className="text-gray-800">{onlinePeople[userId]}</span>
-      </div>
+        <div className="flex gap-2 py-2 pl-4 items-center">
+            <Avatar username={onlinePeople[userId]} userId={userId} />
+            <span className="text-gray-800">{onlinePeople[userId]}</span>
+        </div>
       
     </div>
   ))}
-        </div>
+</div>
 
 
 
@@ -82,16 +91,21 @@ const Chat = () => {
 
 
 
-        <div className="flex gap-2">
+        <form className="flex gap-2" onSubmit={sendMessage}>
             <input type="text" 
+            value={newMessgaeText}
+            onChange={ev => setNewMessageText(ev.target.value)}
                 placeholder="Type Your message"  
                 className="bg-white flex-grow border p-2 rounded-sm"/>
-                <button className="bg-blue-500 p-2 text-white rounded-sm">
+                <button type="submit" className="bg-blue-500 p-2 text-white rounded-sm">
                     <svg xmlns="http://www.w3.org/2000/ svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
                     </svg>
                 </button>
-        </div>
+        </form>
+
+
+
       </div>
     </div>
   )
